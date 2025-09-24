1fc203fcd6a55cdd235688affc04b4048d81b8d7
Mariyah-Curb <mariyahrene@gmail.com>
2025-09-24 16:15:14 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ mrcurb_assignment_4.py
@@ -0,0 +1,16 @@
+student_name = "Mariyah Curb"  # Replace with your actual name
+current_gpa = 3.04 # Float between 1.0-4.0
+study_hours = int(20) # Integer (Ex. 25)
+social_points = int(85) # Integer (Ex. 50)
+stress_level = int(60) # Integer 0-100
+
+# Display Welcome Stats
+
+print(f"------ Welcome {student_name} ------")
+print("")
+print(f"    ---- Your Stats Are: ----")
+print("")
+print(f"        Current GPA: {current_gpa:.1f}")
+print(f"        Study Hours: {study_hours}")
+print(f"        Social Points: {social_points}")
+print(f"        Stress Level: {stress_level}")
