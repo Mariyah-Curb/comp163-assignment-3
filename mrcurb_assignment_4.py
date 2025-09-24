f29f242f3525b670b9a259c7310bccbe6a4852cd
Mariyah-Curb <mariyahrene@gmail.com>
2025-09-24 16:54:47 -0400

    Pass Test Case 3: Study strategy with membership and logical operators

--- mrcurb_assignment_4.py
+++ mrcurb_assignment_4.py
@@ -14,3 +14,38 @@ print(f"        Current GPA: {current_gpa:.1f}")
 print(f"        Study Hours: {study_hours}")
 print(f"        Social Points: {social_points}")
 print(f"        Stress Level: {stress_level}")
+
+#My mom loves Harry potter this is for you Mum!
+print(f"Choose your Wizardy Path!:")
+print(f"A). Potions (Moderate)")
+print(f"B). SpellCaster (Difficult)")
+print(f"C). Curses (Brutal)")
+
+choice = input("Your choice: ")
+
+if choice == "A" or choice == "a":
+    #path = potions
+    if current_gpa >= 2.7:
+        stress_level += 8
+        study_hours +=4
+    elif current_gpa < 2.7:
+        stress_level += 15
+        study_hours += 8
+elif choice == "B"or choice == "b":
+    #path = spells
+    if current_gpa >= 3.0:
+        stress_level += 8
+        study_hours +=4
+    elif current_gpa < 3.0:
+        stress_level += 15
+        study_hours += 8
+elif choice == "C"or choice == "c":
+    if current_gpa >= 3.5:
+        stress_level += 8
+        study_hours += 4
+    elif current_gpa < 3.5:
+        stress_level += 15
+        study_hours += 8
+    #path = curses
+else:
+    print("Invalid choice, Please Restart")
